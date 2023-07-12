b7468f9a01612d86b4d6a79de8f07ded051adb4e
Zen Visibee <visibleone.zen@gmail.com>
2023-07-12 17:50:11 +0630

    feat: my account fetch and a bit responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,117 +1,221 @@
 const emailType = {
   type: String,
-  format: "email"
-}
+  format: "email",
+};
 const googleType = {
   type: String,
-  format: "[google-recaptcha]"
-}
-
-const req_post_account = {
-  coreloy_salutation1: {
-    type: Number,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  coreloy_chinesesalutation: {
-    type: Number,
-  },
-  new_lastnamechinese: {
-    type: String,
-  },
-  new_firstnamechinese: {
-    type: String,
-  },
-  telephone1: {
-    type: String,
-    required: true,
-  },
-  emailaddress1: {
-    type: String,
-    required: true,
-  },
-  jobtitle: {
-    type: String,
-    required: true,
-  },
-  new_positionchinese: {
-    type: String,
-  },
-  new_websitecompanyname: {
-    type: String,
-    required: true,
-  },
-  new_websitecompanynamechinese: {
-    type: String,
-  },
-  new_websiteaccountstatus: {
-    type: Number,
-    default: 100000001,
-  },
-  new_websitepassword: {
-    type: String,
-    required: true,
-  },
-  new_websiteaccountcreationdate: {
-    type: Date,
-    default: Date.now,
-  },
-  coreloy_consent: {
-    type: Boolean,
-  },
+  format: "[google-recaptcha]",
+};
+const cookieType = {
+  "[cookie-in-headers]":
+    "personalization=<token>; Path=<path>(optional); Expires=<date>(optional); etc...",
 };
 
+const req_post_account = {
+  form: {
+    _id: {
+      type: String,
+      default: "uuid.v1",
+    },
+    contactid: {
+      type: String,
+      default: "uuid.v1",
+    },
+    coreloy_salutation1: {
+      type: Number,
+    },
+    coreloy_chinesesalutation: {
+      type: Number,
+    },
+    coreloy_consent: {
+      type: Boolean,
+    },
+    coreloy_salutation1: {
+      type: Number,
+    },
+    emailaddress1: {
+      type: String,
+    },
+    firstname: {
+      type: String,
+    },
+    jobtitle: {
+      type: String,
+    },
+    lastname: {
+      type: String,
+    },
+    new_firstnamechinese: {
+      type: String,
+    },
+    new_lastnamechinese: {
+      type: String,
+    },
+    new_positionchinese: {
+      type: String,
+    },
+    new_websiteaccountcreationdate: {
+      type: Date,
+      default: Date.now,
+    },
+    new_websiteaccountstatus: {
+      type: Number,
+      default: 100000001,
+    },
+    new_websitecompanyname: {
+      type: String,
+    },
+    new_websitecompanynamechinese: {
+      type: String,
+    },
+    new_websitepassword: {
+      type: String,
+    },
+    telephone1: {
+      type: String,
+    },
+    statecode: {
+      type: Number,
+      default: 0,
+    },
+    address1_line1: {
+      type: String,
+    },
+    address1_line2: {
+      type: String,
+    },
+    new_districtlookup: {
+      type: String,
+    },
+    new_arealookup: {
+      type: String,
+    },
+    coreloy_company1: {
+      type: String,
+    },
+  },
+  google: googleType,
+};
 const req_post_account_activate = {
   token: {
     type: String,
-    required: true
-  }
-}
+  },
+};
 
 const req_post_account_session = {
   form: {
     username: emailType,
     password: {
-      type: String
+      type: String,
     },
     consent: {
-      type: Boolean
-    }
+      type: Boolean,
+    },
   },
-  google: googleType
-}
+  google: googleType,
+};
 
-const req_delete_account_session = {
-  "[cookie-in-headers]": {
-    personalization: "[cookie value]"
-  }
-}
+const req_delete_account_session = cookieType;
 
 const req_post_reset_password_send_email = {
   email: emailType,
-  google: googleType
-}
+  google: googleType,
+};
 
 const req_post_reset_password_confirm = {
   token: {
-    type: String
+    type: String,
   },
   password: {
-    type: String
+    type: String,
   },
   reEnterPassword: {
-    type: String
-  }
-}
+    type: String,
+  },
+};
+
+const req_get_accout_my = cookieType;
+const req_patch_accout_my = {
+  form: {
+    isFhkiMember: {
+      type: Boolean,
+    },
+    membershipNo: {
+      type: String,
+    },
+    companyName: {
+      en: {
+        type: String,
+      },
+      tc: {
+        type: String,
+      },
+    },
+    name: {
+      en: {
+        familyName: {
+          type: String,
+        },
+        givenName: {
+          type: String,
+        },
+        title: {
+          type: String,
+        },
+      },
+      tc: {
+        familyName: {
+          type: String,
+        },
+        givenName: {
+          type: String,
+        },
+        title: {
+          type: String,
+        },
+      },
+    },
+    address: {
+      en: [
+        {
+          type: String,
+        },
+        {
+          type: String,
+        },
+        {
+          type: String,
+        },
+        {
+          type: String,
+        },
+      ],
+    },
+    jobTitle: {
+      en: {
+        type: String,
+      },
+      tc: {
+        type: String,
+      },
+    },
+    tel: {
+      type: String,
+    },
+    consent: {
+      type: Boolean,
+    },
+  },
+  google: googleType,
+};
+
+const req_get_account_my_enrolment_form = cookieType;
+const req_get_account_my_attendance = cookieType;
+
+const { ref } = Vue;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     resources: [
       "POST - /account",
@@ -119,7 +223,11 @@ new Vue({
       "POST - /account/session",
       "DELETE - /account/session",
       "POST - /account/reset-password/send-email",
-      "POST - /account/reset-password/confirm"
+      "POST - /account/reset-password/confirm",
+      "GET - /account/my",
+      "PATCH - /account/my",
+      "GET - /accout/my/enrolment-form",
+      "GET - /account/my/attendance"
     ],
     schemas: [
       req_post_account,
@@ -127,14 +235,30 @@ new Vue({
       req_post_account_session,
       req_delete_account_session,
       req_post_reset_password_send_email,
-      req_post_reset_password_confirm
+      req_post_reset_password_confirm,
+      req_get_accout_my,
+      req_patch_accout_my,
+      req_get_account_my_enrolment_form,
+      req_get_account_my_attendance
     ],
-    currentIndex: 0
+    currentIndex: 0,
+    sectionMaxWidth: 0,
   },
   methods: {
     handleClick(index) {
       // Handle the click event here
       this.currentIndex = index;
+    },
+    handleSectionMaxWidth() {
+      const aside = this.$refs.aside;
+      this.sectionMaxWidth = window.innerWidth - aside.getBoundingClientRect().width;
     }
+  },
+  mounted() {
+    this.handleSectionMaxWidth();
+    window.addEventListener('resize', this.handleSectionMaxWidth);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleSectionMaxWidth);
   }
 });
